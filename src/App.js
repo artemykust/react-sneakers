f240d7c6bc7f2d7f7f73cb2ae387c9daf2129034
artemykust <55203801+artemykust@users.noreply.github.com>
2022-11-06 22:35:33 +0400

    Добавление некоторой логики карточке товара (#2)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import Card from './components/Card'
 import Header from './components/Header'
 import Drawer from './components/Drawer'
 
+//! Временный блок с данными вместо бэкэнда
+const arr =[
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 12999,
+    imageUrl: '/img/sneakers/1.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Air Max 270',
+    price: 15599,
+    imageUrl: '/img/sneakers/2.jpg'
+  },
+  {
+    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: 8499,
+    imageUrl: '/img/sneakers/3.jpg'
+  },
+  { 
+    title: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: 8999,
+    imageUrl: '/img/sneakers/4.jpg'
+  }
+]
+//!
+
 function App() {
   return (
     <div className="wrapper">
@@ -16,10 +41,13 @@ function App() {
           </div>
         </div>
         <div className="sneakers">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.title}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+            />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,18 @@
-function Card() {
+function Card(props) {
   return(
     <div className="card">
       <div className="favorite">
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="cardBottom">
         <div >
           <span>Цена:</span>
-          <b>₽12 999</b>
+          <b>₽{props.price}</b>
         </div>
         <button className="button">
-          <img width={11} height={11} src="/img/plus.svg" alt="" />
+          <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
     </div>
