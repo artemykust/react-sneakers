7b74a9ff4fce4be09a2234ff3e74de827ed5d935
artemykust <55203801+artemykust@users.noreply.github.com>
2022-11-13 21:27:45 +0400

    Логика карточек товаров (#5)
    * Загрузка картинок кроссовок для карточек
    
    * Получение данных с бэкэнда (mockapi) и стили карточек

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
-import React from 'react'
-import Card from './components/Card'
-import Header from './components/Header'
-import Drawer from './components/Drawer'
-
-//! Временный блок с данными вместо бэкэнда
-const arr = [
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 12999,
-    imageUrl: '/img/sneakers/1.jpg'
-  },
-  {
-    title: 'Мужские Кроссовки Nike Air Max 270',
-    price: 15599,
-    imageUrl: '/img/sneakers/2.jpg'
-  },
-  {
-    title: 'Мужские Кроссовки Nike Blazer Mid Suede',
-    price: 8499,
-    imageUrl: '/img/sneakers/3.jpg'
-  },
-  {
-    title: 'Кроссовки Puma X Aka Boku Future Rider',
-    price: 8999,
-    imageUrl: '/img/sneakers/4.jpg'
-  }
-]
-//!
+import React from "react";
+import Card from "./components/Card";
+import Header from "./components/Header";
+import Drawer from "./components/Drawer";
 
 function App() {
-  const [cartOpened, setCartOpened] = React.useState(false)
+  const [items, setItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch("https://636ff93b8ac36cc3e7e016e1.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
 
   return (
     <div className="wrapper">
@@ -44,13 +30,13 @@ function App() {
           </div>
         </div>
         <div className="sneakers">
-          {arr.map((obj) => (
+          {items.map((obj) => (
             <Card
               title={obj.title}
               price={obj.price}
               imageUrl={obj.imageUrl}
-              onPlus={() => console.log('Plus')}
-              onFavorite={() => console.log('Like')}
+              onPlus={() => console.log("Plus")}
+              onFavorite={() => console.log("Like")}
             />
           ))}
         </div>
