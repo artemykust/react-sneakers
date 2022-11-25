c4e2d47ef1285718a9870e6abad1b3ac8ca4781f
artemykust <55203801+artemykust@users.noreply.github.com>
2022-11-25 16:08:09 +0400

    Добавлено состояние пустой корзины (#6)
    Реализовано добавление и удаление карточек товара в корзину на стороне бэка

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,24 +1,25 @@
 import styles from "./Card.module.scss";
 import React from "react";
 
-function Card(props) {
+function Card({ title, price, imageUrl, onFavorite, onPlus }) {
   const [isAdded, setIsAdded] = React.useState(false);
 
   const onClickPlus = () => {
+    onPlus({ title, price, imageUrl });
     setIsAdded(!isAdded);
   };
 
   return (
     <div className={styles.card}>
-      <div className={styles.favorite} onClick={props.onFavorite}>
+      <div className={styles.favorite} onClick={onFavorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
-      <h5>{props.title}</h5>
+      <img width={133} height={112} src={imageUrl} alt="Sneakers" />
+      <h5>{title}</h5>
       <div className={styles.cardBottom}>
         <div>
           <span>Цена:</span>
-          <b>₽{props.price}</b>
+          <b>₽{price}</b>
         </div>
         <img
           className={styles.plus}
